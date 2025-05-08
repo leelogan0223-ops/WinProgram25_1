d112800a7520fa3a65e100064f2f6aec39130275
elflee77 <elflee77@gmail.com>
2025-05-08 11:51:05 +0900

    .

--- Lecture10/main.c
+++ Lecture10/main.c
@@ -1,20 +1,15 @@
+#pragma comment(lib, "Opengl32.lib")
+
 #include <GLFW/glfw3.h>
 
-void draw_triangle() {
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(-0.5f, -0.5f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(0.5f, -0.5f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex2f(0.0f, 0.5f);
-    glEnd();
-}
+
+
+/* NuGet 패키지 매니져에서 GLFW 설치후 사용*/
 
 int main() {
     if (!glfwInit()) return -1;
 
-    GLFWwindow* window = glfwCreateWindow(500, 500, "GLFW: 삼각형", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(500, 500, "GLFW: Triangle", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -25,7 +20,14 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        draw_triangle();
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.0f, 0.5f);
+        glEnd();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
