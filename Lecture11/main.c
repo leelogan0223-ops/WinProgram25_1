da001096d03d650ab632a318ba3de51f31ec9291
elflee77 <elflee77@gmail.com>
2025-05-15 10:42:11 +0900

    .

---
+++ Lecture11/main.c
@@ -0,0 +1,134 @@
+#include <GLFW/glfw3.h>
+#include <math.h>
+
+#define PI 3.1415926535f
+
+typedef struct
+{
+    float x, y;
+} Vec2;
+
+typedef struct
+{
+    Vec2 position;
+    float rotation;    // 라디안
+    Vec2 scale;
+} Transform;
+
+typedef struct
+{
+    Transform transform;
+} Triangle;
+
+void draw_triangle(Triangle* tri)
+{
+    glPushMatrix();
+
+    glTranslatef(tri->transform.position.x, tri->transform.position.y, 0.0f);
+    glRotatef(tri->transform.rotation * (180.0f / PI), 0.0f, 0.0f, 1.0f);
+    glScalef(tri->transform.scale.x, tri->transform.scale.y, 1.0f);
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+
+    glPopMatrix();
+}
+
+typedef struct
+{
+    Transform transform;
+} Square;
+
+void draw_square(Square* sq)
+{
+    glPushMatrix();
+
+    glTranslatef(sq->transform.position.x, sq->transform.position.y, 0.0f);
+    glRotatef(sq->transform.rotation * (180.0f / PI), 0.0f, 0.0f, 1.0f);
+    glScalef(sq->transform.scale.x, sq->transform.scale.y, 1.0f);
+
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(-0.5f, 0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(-0.5f, -0.5f);
+    glEnd();
+
+    glPopMatrix();
+}
+
+typedef struct
+{
+    Transform transform;
+    int segments;
+} Circle;
+
+void draw_circle(Circle* c)
+{
+    glPushMatrix();
+
+    glTranslatef(c->transform.position.x, c->transform.position.y, 0.0f);
+    glRotatef(c->transform.rotation * (180.0f / PI), 0.0f, 0.0f, 1.0f);
+    glScalef(c->transform.scale.x, c->transform.scale.y, 1.0f);
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(0.0f, 0.0f);
+
+    for (int i = 0; i <= c->segments; ++i)
+    {
+        float angle = 2.0f * PI * i / c->segments;
+        glVertex2f(cosf(angle) * 0.5f, sinf(angle) * 0.5f);
+    }
+
+    glEnd();
+
+    glPopMatrix();
+}
+
+int main()
+{
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL C Demo", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glOrtho(-1, 1, -1, 1, -1, 1); // 2D
+
+    Triangle tri = { {{0, 0}, 0, {1, 1}} };
+    Square sq = { {{-0.5f, -0.5f}, 0, {1, 1}} };
+    Circle cir = { {{0.5f, 0.5f}, 0, {1, 1}}, 32 };
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float time = (float)glfwGetTime();
+
+        // 애니메이션 예시
+        tri.transform.position.x = sinf(time);
+        sq.transform.rotation = time;
+        cir.transform.scale.x = 1.0f + 0.5f * sinf(time);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        draw_triangle(&tri);
+        draw_square(&sq);
+        draw_circle(&cir);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
